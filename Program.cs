52127a2bfea0d8ce364db76589829221c42242a6
Caroline Ramos <carolbeatrice@gmail.com>
2024-03-08 19:59:08 -0300

    adicionados arquivos csproj e obj + extraida funcao calcula area do main

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace ProblemaTriangulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double xA, xB, xC, yA, yB, yC; //lados dos triangulos
+            Console.WriteLine("Informe os lados do triangulo X (um por vez: ")
+            xA = double.Parse(Console.ReadLine());
+            xB = double.Parse(Console.ReadLine());
+            xC = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe os lados do triangulo Y: ")
+            yA = double.Parse(Console.ReadLine());
+            yB = double.Parse(Console.ReadLine());
+            yC = double.Parse(Console.ReadLine());
+
+
+            double areaX = CalculoAreaTriangulo(xA, xB, xC);
+
+            double areaY = CalculoAreaTriangulo(yA, yB, yC);
+
+            Console.WriteLine($"Valor área X: {areaX.ToString("F2")}");
+            Console.WriteLine($"Valor área y: {areaY.ToString("F2")}");
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Area de X é maior");
+            }
+            else
+            {
+                Console.WriteLine("Area de Y é maior");
+            }
+
+        }
+
+        public static double CalculoAreaTriangulo(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area
         }
     }
 }
